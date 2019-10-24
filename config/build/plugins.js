84ce93ced002e5cd956ce20897adf65f10f4bd38
Nikita Karamov <nick@karamoff.ru>
2019-10-24 14:02:56 +0200

    Remove banner from final build

--- config/build/plugins.js
+++ config/build/plugins.js
@@ -1,15 +1,6 @@
-import rpBanner from 'rollup-plugin-banner';
 import rpStrip from 'rollup-plugin-strip';
 import { terser as rpTerser } from 'rollup-plugin-terser';
 import rpTypescript2 from 'rollup-plugin-typescript2';
-
-export const banner = () => rpBanner(
-    'pretty-money\n' +
-    '<%= pkg.description %>\n' +
-    '\n' +
-    '@version <%= pkg.version %>\n' +
-    '@license MIT'
-);
 
 export const strip = () => rpStrip({
     debugger: true,

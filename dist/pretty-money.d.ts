c93ea246b5dc9131f6ffb2cd568ff43774d930d0
Nikita Karamov <nick@karamoff.ru>
2019-10-24 13:32:10 +0200

    Add automatic generation of typings file

--- dist/pretty-money.d.ts
+++ dist/pretty-money.d.ts
@@ -5,12 +5,10 @@ interface FormatOptions {
      * It can be the respective sign (like "$"), currency code (like "GBP") or a word (like "peso").
      */
     currency?: string;
-
     /**
      * A string that separates the integer and the fraction parts of the number.
      */
     decimalDelimiter?: string;
-
     /**
      * Sets the amount of decimal places.
      *
@@ -18,71 +16,28 @@ interface FormatOptions {
      * - "fluid" — the amount of places will stay at any number between `minDecimal` and `maxDecimal`, in order not to have trailing zeros.
      * - "minmax" — the amount of places will stay at `maxDecimal` unless it's possible to be at `minDecimal` without having trailing zeros.
      */
-    decimals?: "fluid"|"minmax"|"fixed";
-
+    decimals?: "fluid" | "minmax" | "fixed";
     /**
      * The maximum number of decimal places allowed in the number.
      */
     maxDecimal?: number;
-
     /**
      * The minimum number of decimal places allowed in the number. Has no effect when `decimals` is `"fixed"`.
      */
     minDecimal?: number;
-
     /**
      * Sets the position of the currency symbol with respect to the number.
      */
-    position?: "before"|"after";
-
+    position?: "before" | "after";
     /**
      * Sets whether there should be a space between the number and the currency symbol.
      */
     spaced?: boolean;
-
     /**
      * A string that separates the thousands of the number.
      */
     thousandsDelimiter?: string;
 }
-
-type prettifyFactory = {
-    (options: FormatOptions, number: number|string): string;
-    (options: FormatOptions): prettify;
-    (): prettify;
-}
-
-type prettify = {
-    (number: number|string): string;
-}
-
-
-/**
- * Prettifies a number according to the given format or returns a curried function to prettify any number
- *
- * ## Usage
- * ```
- * > prettify({currency: "USD"}, 5);
- * 5 USD
- *
- * > const euros = prettify({currency: "EUR"});
- * > euros("12.345")
- * 12.34 EUR
- *
- * > const rubles = prettify({
- *     currency: "₽",
- *     decimals: "fixed",
- *     decimalDelimiter: ",",
- *     thousandsDelimiter: " "
- * });
- * > rubles(56789)
- * 56 789,00 ₽
- * ```
- *
- * @param options - formatting options
- * @param number - the number to be currency-formatted
- * @returns the format results, if the number was provided, or a formatting function otherwise
- */
-declare const prettyMoney: prettify;
-
+declare function prettyMoney(options: FormatOptions): Function;
+declare function prettyMoney(options: FormatOptions, number: number | string): string;
 export default prettyMoney;

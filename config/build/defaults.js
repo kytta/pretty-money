84ed10a3a7ce5d550e97e3f88600f01889c26def
Nikita Karamov <nick@karamoff.dev>
2021-03-20 14:32:38 +0100

    Migrate from Rollup to Microbundle
    It provides a simpler config

--- config/build/defaults.js
+++
@@ -1,7 +0,0 @@
-export const input = "./src/pretty-money.js";
-
-export const defaultOutput = {
-    name: "prettyMoney",
-};
-
-export const getFilename = (infix) => `./dist/pretty-money.${infix}.js`;
